990a58ee0af92796c024be067e351301d89a32b8
shlee3552 <mr_shield@cpst.co.kr>
2021-02-09 14:25:07 +0900

    클라이언트와 axios 통신 연결 완료

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,19 @@
+
 var express = require('express');
 var router = express.Router();
+const axios = require("axios");
+//읽어온 정보를 html로직으로 변환하여 화면작업을 하도록 유도합니다
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.post('/', function(req, res, next) {
+  let url = '크롤링 주소';
+  
+  axios.get(url).then(html => {
+    console.log('req.body = ',req.body )
+    // console.log('html.data = ',html.data)
+    
+    return res.json('data');
+  })
 });
 
 module.exports = router;
